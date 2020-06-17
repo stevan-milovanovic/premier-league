6a020579b57a0561dd38d6955c0df322f4b69688
Stevan Milovanovic <stevanm993@gmail.com>
2020-06-17 23:07:54 +0200

    auto cleared value bug fixed

--- app/src/main/java/com/smobile/premierleague/util/AutoClearedValue.kt
+++ app/src/main/java/com/smobile/premierleague/util/AutoClearedValue.kt
@@ -1,6 +1,9 @@
 package com.smobile.premierleague.util
 
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
@@ -11,16 +14,25 @@ import kotlin.reflect.KProperty
  */
 class AutoClearedValue<T : Any>(val fragment: Fragment) : ReadWriteProperty<Fragment, T> {
 
-    private var value: T? = null
+    private var _value: T? = null
+
+    init {
+        fragment.lifecycle.addObserver(object : LifecycleObserver {
+            @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+            fun onDestroy() {
+                _value = null
+            }
+        })
+    }
 
     override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
-        return value ?: throw IllegalStateException(
+        return _value ?: throw IllegalStateException(
             "should never call auto-cleared-value get when it might not be available"
         )
     }
 
     override fun setValue(thisRef: Fragment, property: KProperty<*>, value: T) {
-        this.value = value
+        this._value = value
     }
 
 }
