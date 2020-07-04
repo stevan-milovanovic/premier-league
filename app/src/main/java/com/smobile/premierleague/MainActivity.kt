6f287bb1ed471430a968cb607cfb7f45a6443534
Stevan Milovanovic <stevanm993@gmail.com>
2020-07-04 21:57:43 +0200

    localisation with attach base context

--- app/src/main/java/com/smobile/premierleague/MainActivity.kt
+++ app/src/main/java/com/smobile/premierleague/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.smobile.premierleague
 
-import android.content.SharedPreferences
+import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import androidx.preference.PreferenceManager
 import com.smobile.premierleague.Const.LANGUAGE
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
@@ -17,12 +19,22 @@ class MainActivity : AppCompatActivity(), HasAndroidInjector {
     @Inject
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
 
-    @Inject
-    lateinit var sharedPreferences: SharedPreferences
+    override fun attachBaseContext(newBase: Context?) {
+        val configuration = Configuration()
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(newBase)
+        sharedPreferences.getString(LANGUAGE, null)?.let {
+            configuration.setLocale(Locale(it))
+        } ?: configuration.setLocale(Locale.getDefault())
+
+        //Workaround for platform bug on SDK < 26
+        configuration.fontScale = 0f
+
+        val updatedContext = newBase?.createConfigurationContext(configuration)
+        super.attachBaseContext(updatedContext)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setLanguage()
         setContentView(R.layout.activity_main)
         val navigationController = findNavController(R.id.fragment_container)
         NavigationUI.setupActionBarWithNavController(this, navigationController)
@@ -34,16 +46,5 @@ class MainActivity : AppCompatActivity(), HasAndroidInjector {
     }
 
     override fun androidInjector(): AndroidInjector<Any> = dispatchingAndroidInjector
-
-    private fun setLanguage() {
-        val dm = resources.displayMetrics
-        val conf = resources.configuration
-
-        sharedPreferences.getString(LANGUAGE, null)?.let {
-            conf.setLocale(Locale(it))
-        } ?: conf.setLocale(Locale.getDefault())
-
-        resources.updateConfiguration(conf, dm)
-    }
 
 }
--- app/src/main/java/com/smobile/premierleague/di/AppModule.kt
+++ app/src/main/java/com/smobile/premierleague/di/AppModule.kt
@@ -2,8 +2,7 @@ package com.smobile.premierleague.di
 
 import android.app.Application
 import android.content.SharedPreferences
-import androidx.security.crypto.EncryptedSharedPreferences
-import androidx.security.crypto.MasterKey
+import androidx.preference.PreferenceManager
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -15,15 +14,7 @@ class AppModule {
     @Singleton
     @Provides
     fun getSharedPrefs(app: Application): SharedPreferences {
-        return EncryptedSharedPreferences.create(
-            app,
-            "secret_shared_prefs_palmhr",
-            MasterKey.Builder(app, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
-                .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
-                .build(),
-            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-        )
+        return PreferenceManager.getDefaultSharedPreferences(app)
     }
 
     @Singleton
