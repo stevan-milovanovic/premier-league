808d6666408999f1ee03d6b05d71e962b33e6062
Stevan Milovanovic <stevanm993@gmail.com>
2020-07-04 21:47:46 +0200

    encrypted shared preferences implementation

--- app/src/main/java/com/smobile/premierleague/MainActivity.kt
+++ app/src/main/java/com/smobile/premierleague/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.smobile.premierleague
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
-import androidx.preference.PreferenceManager
 import com.smobile.premierleague.Const.LANGUAGE
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
@@ -17,9 +17,12 @@ class MainActivity : AppCompatActivity(), HasAndroidInjector {
     @Inject
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
 
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        setLanguage()
         super.onCreate(savedInstanceState)
+        setLanguage()
         setContentView(R.layout.activity_main)
         val navigationController = findNavController(R.id.fragment_container)
         NavigationUI.setupActionBarWithNavController(this, navigationController)
@@ -33,13 +36,12 @@ class MainActivity : AppCompatActivity(), HasAndroidInjector {
     override fun androidInjector(): AndroidInjector<Any> = dispatchingAndroidInjector
 
     private fun setLanguage() {
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
         val dm = resources.displayMetrics
         val conf = resources.configuration
-        val lang = sharedPreferences.getString(LANGUAGE, null)
-        if (lang == null) {
-            conf.setLocale(Locale.getDefault())
-        } else conf.setLocale(Locale(lang))
+
+        sharedPreferences.getString(LANGUAGE, null)?.let {
+            conf.setLocale(Locale(it))
+        } ?: conf.setLocale(Locale.getDefault())
 
         resources.updateConfiguration(conf, dm)
     }
--- app/src/main/java/com/smobile/premierleague/di/AppModule.kt
+++ app/src/main/java/com/smobile/premierleague/di/AppModule.kt
@@ -2,7 +2,8 @@ package com.smobile.premierleague.di
 
 import android.app.Application
 import android.content.SharedPreferences
-import androidx.preference.PreferenceManager
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -14,7 +15,15 @@ class AppModule {
     @Singleton
     @Provides
     fun getSharedPrefs(app: Application): SharedPreferences {
-        return PreferenceManager.getDefaultSharedPreferences(app)
+        return EncryptedSharedPreferences.create(
+            app,
+            "secret_shared_prefs_palmhr",
+            MasterKey.Builder(app, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
+                .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+                .build(),
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
     }
 
     @Singleton
